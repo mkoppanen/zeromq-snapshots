adea5636b12d488e0d17714668b1029b11a587a1
Mikko Koppanen <mkoppanen@php.net>
2011-10-11 00:49:13 +0100

    Maybe move under windows

--- application/config/snapshot.php
+++ application/config/snapshot.php
@@ -6,15 +6,13 @@ $config ['zip_ttl'] = 86400 * 10;
 $config ['os'] ['win64']  = 'Windows 64 bit';
 $config ['os'] ['win32']  = 'Windows 32 bit';
 $config ['os'] ['src']    = 'Source packages';
-$config ['os'] ['phpwin'] = 'PHP 5.3 extension';
 
 $config ['path'] ['src'] ['2.1-stable'] = '/home/jenkins/snapshot/src/libzmq2-1';
 $config ['path'] ['src'] ['3.0-beta']   = '/home/jenkins/snapshot/src/libzmq3-0/';
 
 $config ['path'] ['win64'] ['2.1-stable'] = '/home/jenkins/snapshot/win64/libzmq2-1';
 $config ['path'] ['win32'] ['2.1-stable'] = '/home/jenkins/snapshot/win32/libzmq2-1';
-
-$config ['path'] ['phpwin'] ['zeromq-2.1'] = '/home/jenkins/snapshot/phpwin';
+$config ['path'] ['win32'] ['php53-ext']  = '/home/jenkins/snapshot/win32/php';
 
 #$config ['path'] ['win64'] ['3.0-beta'] = APPPATH . 'data/x1/test';
 #$config ['path'] ['win32'] ['3.0-beta'] = APPPATH . 'data/x2/test';
